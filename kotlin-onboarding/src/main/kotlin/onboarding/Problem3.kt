2b515dea4502cafebb888ed300b62938c0325d50
Donghyun Chae <asena@Donghyuns-Mac-mini.local>
2025-04-05 20:48:13 +0900

    onboarding-problem3

--- kotlin-onboarding/src/main/kotlin/onboarding/Problem3.kt
+++ kotlin-onboarding/src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,9 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var result = 0
+    for (n in 1..number) {
+        n.toString().map { acc -> if (acc == '3' || acc == '6' || acc == '9') {result += 1 }}
+    }
+    return result
 }
