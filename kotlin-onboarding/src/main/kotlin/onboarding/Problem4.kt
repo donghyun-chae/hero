66832d43084b5e4fb29cd53271df15ced512c634
Donghyun Chae <asena@Donghyuns-Mac-mini.local>
2025-04-05 21:53:48 +0900

    refactor onboarding-problem4

--- kotlin-onboarding/src/main/kotlin/onboarding/Problem4.kt
+++ kotlin-onboarding/src/main/kotlin/onboarding/Problem4.kt
@@ -1,15 +1,11 @@
 package onboarding
 
 fun solution4(word: String): String {
-    var result = ""
-    word.forEach { acc -> when {
-        acc.isUpperCase() -> result += ('Z'.code - (acc.code - 'A'.code)).toChar()
-        acc.isLowerCase() -> result += ('z'.code - (acc.code - 'a'.code)).toChar()
-        acc.isWhitespace() -> result += " "
-    }}
-    return result
-}
-
-fun main() {
-    print(solution4("I love you"))
+    return buildString {
+        word.forEach { acc -> when {
+            acc.isUpperCase() -> append(('Z'.code - (acc.code - 'A'.code)).toChar())
+            acc.isLowerCase() -> append(('z'.code - (acc.code - 'a'.code)).toChar())
+            acc.isWhitespace() -> append(" ")
+        }}
+    }
 }
