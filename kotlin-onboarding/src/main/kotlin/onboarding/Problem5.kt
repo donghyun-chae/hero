8933250926b1f142f06dc7306bdbd2854dba474e
Donghyun Chae <asena@Donghyuns-Mac-mini.local>
2025-04-05 22:20:44 +0900

    onboarding-problem5

--- kotlin-onboarding/src/main/kotlin/onboarding/Problem5.kt
+++ kotlin-onboarding/src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,10 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    val curr = listOf(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1)
+    return curr.fold(Pair(money, mutableListOf<Int>())) { (remaining, acc), c ->
+        val count = remaining / c
+        acc.add(count)
+        Pair(remaining - count * c, acc)
+    }.second
 }
